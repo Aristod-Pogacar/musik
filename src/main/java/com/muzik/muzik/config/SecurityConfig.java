22bdc8994c119a61d84163bbb76e0591b2d0bd0f
tynaandritahina <tynaandritahina@gmail.com>
2025-12-04 20:19:20 +0300

    redirect

--- src/main/java/com/muzik/muzik/config/SecurityConfig.java
+++ src/main/java/com/muzik/muzik/config/SecurityConfig.java
@@ -16,27 +16,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable())
 
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login", "/users/new", "/css/**", "/js/**").permitAll()
-                .anyRequest().authenticated()
-            )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/users/new", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated())
 
-            .formLogin(form -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/music", true)
-                .permitAll()
-            )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/music", true)
+                        .permitAll())
 
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .permitAll()
-            );
-
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll());
 
         return http.build();
     }

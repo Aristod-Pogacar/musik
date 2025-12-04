a0dcf2e110779407dbb80de5bb1a07836f0e4b77
tynaandritahina <tynaandritahina@gmail.com>
2025-12-04 20:09:22 +0300

    redirection music listing page

--- src/main/java/com/muzik/muzik/helloController.java
+++ src/main/java/com/muzik/muzik/helloController.java
@@ -5,7 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class helloController {
-    
+
+    @GetMapping("/")
+    public String redirectToMusic() {
+        return "redirect:/music";
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
